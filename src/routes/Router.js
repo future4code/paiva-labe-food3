31771bef2abd962aedf1fa247579daed283540d9
Tiagopb <tiago000@outlook.com>
2021-06-28 15:46:47 -0300

    coordinator

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -3,50 +3,53 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "../pages/HomeHage/HomePage";
 import SignUpPage from "../pages/SignUpPage/SignUpPage";
 import ProfilePage from "../pages/ProfilePage/ProfilePage";
-import Restaurant from "../pages/Restaurant/restaurant";
+import Restaurant from "../pages/Restaurant/Restaurant";
 import SearchPage from "../pages/searchPage/SearchPage.js";
 import LoginPage from "../pages/loginPage/LoginPage";
 import MyCartPage from "../pages/MyCartPage/MyCartPage.js";
 
+
+
+
 const Router = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path={"/"}>
-          <HomePage />
-        </Route>
-
-        <Route exact path={"/login"}>
-          <LoginPage />
-        </Route>
-
-        <Route exact path={"/signup"}>
-          <SignUpPage />
-        </Route>
-
-        <Route exact path={"/Mycart"}>
-        <MyCartPage />
-        </Route>
-
-      
-        <Route exact path={"/search"}>
-          <SearchPage />
-        </Route>
-
-        <Route exact path={"/restaurant/:idRest"}>
-          <Restaurant />
-        </Route>
-
-        <Route exact path={"/Profile"}>
-          <ProfilePage />
-        </Route>
-
-        <Route>
-          <div>Página não encontrada</div>
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
-};
-
-export default Router;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={"/"}>
+            <HomePage />
+          </Route>
+  
+          <Route exact path={"/login"}>
+           <LoginPage />
+             </Route>
+  
+          <Route exact path={"/signup"}>
+            <SignUpPage />
+          </Route>
+  
+  
+          <Route exact path={"/Mycart"}>
+            </Route>
+  
+  
+          <Route exact path={"/search"}>
+            <SearchPage />
+          </Route>
+  
+          <Route exact path={"/restaurant/:idRest"}>
+            <Restaurants />
+          </Route>
+  
+          <Route exact path={"/Profile"}>
+            <ProfilePage />
+          </Route>
+  
+          <Route>
+            <div>Página não encontrada</div>
+          </Route>
+          </Switch>
+          </BrowserRouter>
+    );
+  };
+  
+  export default Router;
