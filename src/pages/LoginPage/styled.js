1a5580441582fb381393d0ebc128612a998a9680
Renato Marinho <renato.marinhofr@gmail.com>
2021-06-28 18:44:13 -0300

    materialUI styling with styled components

---
+++ src/pages/LoginPage/styled.js
@@ -0,0 +1,36 @@
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import styled from "styled-components";
+
+export const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export const Text = styled.p`
+  display: flex;
+  justify-content: center;
+`;
+export const StyledButton = styled(Button)`
+  background: #e8222e;
+  color: black;
+  text-transform: capitalize;
+  &:hover {
+    background: #a21720;
+  }
+`;
