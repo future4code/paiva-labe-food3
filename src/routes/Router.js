664645f0e97e11ac9efa710a3b947d7f7c636a2d
Tiagopb <tiago000@outlook.com>
2021-06-28 14:55:45 -0300

    criado rotas

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,55 +1,45 @@
 const Router = () => {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path={"/"}>
-            <HomePage />
-          </Route>
-  
-          <Route exact path={"/login"}>
-           
-              <LoginPage />
-            
-          </Route>
-  
-          <Route exact path={"/signup"}>
-            <SignUpPage />
-          </Route>
-  
-          <Route exact path={"/addAddress"}>
-            
-          </Route>
-  
-          <Route exact path={"/cart"}>
-            
-          </Route>
-  
-          <Route exact path={"/profile"}>
-            <ProfilePage />
-          </Route>
-  
-          <Route exact path={"/search"}>
-            <SearchPage />
-          </Route>
-  
-          <Route exact path={"/editAddressPage"}>
-            
-          </Route>
-  
-          <Route exact path={"/restaurant/:idRest"}>
-            <Restaurants />
-          </Route>
-  
-          <Route exact path={"/editProfile"}>
-            <EditProfilePage />
-          </Route>
-  
-          <Route>
-            <div>Página não encontrada</div>
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    );
-  };
-  
-  export default Router;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={"/"}>
+          <HomePage />
+        </Route>
+
+        <Route exact path={"/login"}>
+          <LoginPage />
+        </Route>
+
+        <Route exact path={"/signup"}>
+          <SignUpPage />
+        </Route>
+
+        <Route exact path={"/Mycart"}>
+        <MyCartPage />
+        </Route>
+
+        <Route exact path={"/profile"}>
+          <ProfilePage />
+        </Route>
+
+        <Route exact path={"/search"}>
+          <SearchPage />
+        </Route>
+
+        <Route exact path={"/restaurant/:idRest"}>
+          <Restaurant />
+        </Route>
+
+        <Route exact path={"/Profile"}>
+          <ProfilePage />
+        </Route>
+
+        <Route>
+          <div>Página não encontrada</div>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
