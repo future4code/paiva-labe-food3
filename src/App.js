a3e177b39fcde84b306962f8fc6dc6966727e562
dan-ueno <ueno2005@gmail.com>
2021-06-29 12:43:49 -0300

    arrumei esqueleto do global e os imports das pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import Router from "./routes/Router";
-import GlobalState from "./global/GlobalState";
+import {GlobalState} from "./global/GlobalState";
 import { ThemeProvider } from "@material-ui/core";
 import theme from "./constants/theme";
 
---
+++ src/global/GlobalState.js
@@ -0,0 +1,37 @@
+import { GlobalStateContext } from "./GlobalStateContext";
+import React, { useContext, useEffect, useState } from "react";
+
+export const GlobalState = (props) => {
+    
+  const [data, setData] = useState([]);
+  const token = localStorage.getItem("token");
+
+
+
+
+  const states = { data };
+  const setters = { setData };
+
+
+
+
+  return (
+    <GlobalStateContext.Provider value={{ states, setters }}>
+      {props.children}
+    </GlobalStateContext.Provider>
+  );
+};
+
+export const useGlobal = () => {
+  return useContext(GlobalStateContext);
+};
+
+export const useGlobalStates = () => {
+  const { states } = useGlobal();
+  return states;
+};
+
+export const useGlobalSetters = () => {
+  const { setters } = useGlobal();
+  return setters;
+};
