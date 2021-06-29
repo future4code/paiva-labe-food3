63ce13fa73842d351c8f457e391ef97d28491bd7
bruxxi <bruna_nunciato@hotmail.com>
2021-06-29 14:59:06 -0300

    conflict ok

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -6,6 +6,7 @@ import SignUpPage from "../pages/SignupPage/SignupPage";
 import ProfilePage from "../pages/ProfilePage/ProfilePage";
 import SearchPage from "../pages/SearchPage/SearchPage";
 import RestaurantPage from "../pages/RestaurantPage/RestaurantPage";
+import MenuPage from '../pages/MenuPage/MenuPage';
 
 const Router = () => {
   return (
@@ -14,31 +15,26 @@ const Router = () => {
         <Route exact path={"/"}>
           <HomePage />
         </Route>
-
         <Route exact path={"/login"}>
           <LoginPage />
         </Route>
-
         <Route exact path={"/signup"}>
           <SignUpPage />
         </Route>
-
         <Route exact path={"/cadastar-endereco"}></Route>
-
         <Route exact path={"/cart"}></Route>
-
         <Route exact path={"/profile"}>
           <ProfilePage />
         </Route>
-
         <Route exact path={"/search"}>
           <SearchPage />
         </Route>
-
         <Route exact path={"/editAddressPage"}></Route>
-
-        <Route exact path={"/restaurantes"}>
+        <Route exact path={"/restaurant"}>
           <RestaurantPage />
+        </Route>        
+        <Route exact path={"/restaurant/:idRest"}>
+          <MenuPage />
         </Route>
         <Route>
           <div>Página não encontrada</div>
