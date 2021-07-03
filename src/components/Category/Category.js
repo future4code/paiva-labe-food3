937c4f9c7b4499abd84b0810e9fb242ee3bdaf77
dan-ueno <ueno2005@gmail.com>
2021-07-03 12:15:52 -0300

    filho unico corrigido

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -2,18 +2,12 @@ import React, { useState } from "react";
 import { useContext } from "react";
 import { GlobalStateContext } from "../../global/GlobalStateContext";
 
-const Category= (props) => {
-const {categoria,setChoice} = props
-const listadeCategoria = categoria.map((el) => {
-    return(
-        <span>el</span>
-    )
-})
-    
- return (
-     {listadeCategoria}
- )
-
-}  
+const Category = (props) => {
+  const { categoria, setChoice } = props;
+  const listadeCategoria = categoria.map((el) => {
+    return <span onClick={setChoice(el)}>{el}</span>;
+  });
+  return <>{listadeCategoria}</>;
+};
 
 export default Category;
